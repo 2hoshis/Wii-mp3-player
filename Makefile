ae337ed4b684ce0929b41547000e948ca27b7f03
Nigemizuluvheart <coolrunninggunso@gmail.com>
2026-06-19 16:09:41 +0900

    Add Wii hello world source

---
+++ Makefile
@@ -0,0 +1,39 @@
+# Minimal Wii homebrew Makefile for devkitPro/libogc.
+
+TARGET := hello-world
+BUILD := build
+SOURCES := source
+
+DEVKITPRO ?= /opt/devkitpro
+DEVKITPPC ?= $(DEVKITPRO)/devkitPPC
+LIBOGC := $(DEVKITPRO)/libogc
+
+CC := $(DEVKITPPC)/bin/powerpc-eabi-gcc
+ELF2DOL := $(DEVKITPPC)/bin/elf2dol
+
+MACHDEP := -DGEKKO -mrvl -mcpu=750 -meabi -mhard-float
+CFLAGS := -g -O2 -Wall $(MACHDEP) -I$(LIBOGC)/include
+LDFLAGS := -g $(MACHDEP) -L$(LIBOGC)/lib/wii
+LIBS := -lwiiuse -lbte -logc -lm
+
+CFILES := $(wildcard $(SOURCES)/*.c)
+OFILES := $(patsubst $(SOURCES)/%.c,$(BUILD)/%.o,$(CFILES))
+
+.PHONY: all clean
+
+all: $(TARGET).dol
+
+$(BUILD):
+	mkdir -p $(BUILD)
+
+$(BUILD)/%.o: $(SOURCES)/%.c | $(BUILD)
+	$(CC) $(CFLAGS) -c $< -o $@
+
+$(TARGET).elf: $(OFILES)
+	$(CC) $(LDFLAGS) $^ $(LIBS) -o $@
+
+$(TARGET).dol: $(TARGET).elf
+	$(ELF2DOL) $< $@
+
+clean:
+	rm -rf $(BUILD) $(TARGET).elf $(TARGET).dol
---
+++ source/main.c
@@ -0,0 +1,48 @@
+#include <gccore.h>
+#include <stdio.h>
+#include <wiiuse/wpad.h>
+
+static void initialise_video(void)
+{
+    VIDEO_Init();
+
+    GXRModeObj *video_mode = VIDEO_GetPreferredMode(NULL);
+    void *framebuffer = MEM_K0_TO_K1(SYS_AllocateFramebuffer(video_mode));
+
+    console_init(framebuffer, 20, 20, video_mode->fbWidth, video_mode->xfbHeight,
+                 video_mode->fbWidth * VI_DISPLAY_PIX_SZ);
+
+    VIDEO_Configure(video_mode);
+    VIDEO_SetNextFramebuffer(framebuffer);
+    VIDEO_SetBlack(FALSE);
+    VIDEO_Flush();
+    VIDEO_WaitVSync();
+
+    if (video_mode->viTVMode & VI_NON_INTERLACE) {
+        VIDEO_WaitVSync();
+    }
+}
+
+int main(void)
+{
+    initialise_video();
+    WPAD_Init();
+
+    printf("\x1b[2;0H");
+    printf("Hello World from Wii homebrew!\n\n");
+    printf("Run this .dol in Dolphin.\n");
+    printf("Press HOME on a Wii Remote to exit.\n");
+
+    while (1) {
+        WPAD_ScanPads();
+
+        u32 pressed = WPAD_ButtonsDown(0);
+        if (pressed & WPAD_BUTTON_HOME) {
+            break;
+        }
+
+        VIDEO_WaitVSync();
+    }
+
+    return 0;
+}
