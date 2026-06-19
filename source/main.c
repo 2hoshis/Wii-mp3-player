e79b4fbed5fd24f6c1657c9ae63368752156a220
Nigemizuluvheart <coolrunninggunso@gmail.com>
2026-06-19 16:42:03 +0900

    Add simple menu navigation

--- source/main.c
+++ source/main.c
@@ -23,24 +23,80 @@ static void initialise_video(void)
     }
 }
 
+static void draw_menu(int selected)
+{
+    const char *items[] = {
+        "Hello",
+        "Button Test",
+        "Future MP3 Player"
+    };
+
+    int item_count = 3;
+
+    printf("\x1b[2J");
+    printf("\x1b[2;0H");
+
+    printf("Wii Homebrew Practice\n\n");
+
+    for (int i = 0; i < item_count; i++) {
+        if (i == selected) {
+            printf("> %s\n", items[i]);
+        } else {
+            printf("  %s\n", items[i]);
+        }
+    }
+
+    printf("\n");
+    printf("Press UP/DOWN to move.\n");
+    printf("Press A to select.\n");
+    printf("Press HOME to exit.\n");
+}
+
 int main(void)
 {
     initialise_video();
     WPAD_Init();
 
-    printf("\x1b[2;0H");
-    printf("Hello World from Wii homebrew!\n\n");
-    printf("Press A to show a message.\n");
-    printf("Press HOME on a Wii Remote to exit.\n");
+    int selected = 0;
+    int item_count = 3;
+
+    draw_menu(selected);
 
     while (1) {
         WPAD_ScanPads();
 
         u32 pressed = WPAD_ButtonsDown(0);
 
+        if (pressed & WPAD_BUTTON_UP) {
+            selected--;
+
+            if (selected < 0) {
+                selected = item_count - 1;
+            }
+
+            draw_menu(selected);
+        }
+
+        if (pressed & WPAD_BUTTON_DOWN) {
+            selected++;
+
+            if (selected >= item_count) {
+                selected = 0;
+            }
+
+            draw_menu(selected);
+        }
+
         if (pressed & WPAD_BUTTON_A) {
-            printf("\x1b[8;0H");
-            printf("You pressed A!        \n");
+            printf("\x1b[12;0H");
+
+            if (selected == 0) {
+                printf("Selected: Hello             \n");
+            } else if (selected == 1) {
+                printf("Selected: Button Test       \n");
+            } else if (selected == 2) {
+                printf("Selected: Future MP3 Player \n");
+            }
         }
 
         if (pressed & WPAD_BUTTON_HOME) {
